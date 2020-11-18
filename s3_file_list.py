87faa6e848706988eec4319e51b4676d5c2ab8b1
Sandra Chung <sandra@moveon.org>
2020-11-18 13:55:48 -0700

    added a check for nonzero file size

--- s3_file_list.py
+++ s3_file_list.py
@@ -19,7 +19,7 @@ def s3_file_list(args) -> list:
         aws_secret_access_key=args.AWS_SECRET_KEY,
     )
     object_list = s3_client.list_objects(Bucket=args.S3_BUCKET)
-    file_list = [file.get('Key') for file in object_list.get('Contents', [])]
+    file_list = [file.get('Key') for file in object_list.get('Contents', []) if file.get('Size') > 0]
     # We get at most 1000 results at a time
     while len(object_list.get('Contents', []))==1000:
         print("Over 1000 found, getting next 1000")
@@ -27,7 +27,7 @@ def s3_file_list(args) -> list:
         print(f"Marker: {marker}")
         object_list = s3_client.list_objects(Bucket=args.S3_BUCKET, Marker=marker)
         print(f"Found {len(object_list)} more objects")
-        file_list = file_list + [file.get('Key') for file in object_list.get('Contents', [])]
+        file_list = file_list + [file.get('Key') for file in object_list.get('Contents', []) if file.get('Size') > 0]
     return file_list
 
 
